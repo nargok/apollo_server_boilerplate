ce608b63fcaa29a67b6338d54e238d00e8685ed7
nargok <nargok@gmail.com>
2019-01-08 00:29:56 +0900

    install uuid and use it as id of message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
+import uuidv4 from 'uuid/v4';
 
 const app = express();
 
@@ -84,10 +85,15 @@ const resolvers = {
 
   Mutation: {
     createMessage: (parent, { text }, { me }) => {
+      const id  = uuidv4();
       const message = {
+        id,
         text,
-        userId: me,id,
+        userId: me.id,
       };
+
+      messages[id] = message;
+      users[me.id].messageIds.push(id);
 
       return message;
     },
