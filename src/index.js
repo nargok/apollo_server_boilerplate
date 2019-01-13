9cde41aec69787415c7b68f81a8d013c9cdc2a7b
nargok <nargok@gmail.com>
2019-01-13 23:18:54 +0900

    setup postgres and initial data

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { ApolloServer } from 'apollo-server-express';
 
 import schema from './schema';
 import resolvers from './resolvers';
-import models, { sequelize, seaquelize } from './models';
+import models, { seaquelize } from './models';
 
 const app = express();
 
@@ -21,8 +21,47 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app, path: '/graphql' });
 
-seaquelize.sync().then(async () => {
+const eraseDetabaseOnSync = true;
+
+seaquelize.sync({ force: eraseDetabaseOnSync }).then(async () => {
+  if (eraseDetabaseOnSync) {
+    createUserWithMessages();
+  }
+
   app.listen({ port: 8000 }, () => {
     console.log('Apollo Server on http://localhost:8000/graphql');
   });
 });
+
+const createUserWithMessages = async () => {
+  await models.User.create(
+    {
+      username: 'admin',
+      messages: [
+        {
+          text: 'Published the Road to learn React',
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+
+  await models.User.create(
+    {
+      username: 'secondUser',
+      messages: [
+        {
+          text: 'Happy to release ...',
+        },
+        {
+          text: 'Published a complete ...'
+        },
+      ],
+    },
+    {
+      include: [models.Message]
+    },
+  );
+};
